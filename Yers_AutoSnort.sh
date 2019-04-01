6387194394fbe53cb2fde4222760c8be1be8c7e3
yersiniah <mynameismynamelolz@gmail.com>
2019-03-31 20:32:46 -0400

    Add files via upload

--- Yers_AutoSnort.sh
+++ Yers_AutoSnort.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#AutoSnort3 PreReq and Install Script for HomeLab
+#AutoSnort 2.9.12 PreReq and Install Script for HomeLab
 
 mkdir ~/snort_src
 cd ~/snort_src
@@ -62,3 +62,46 @@ ldconfig
 ln -s /usr/local/bin/snort /usr/sbin/snort
 
 /usr/sbin/snort -V
+
+#Basic Snort Configuration
+
+groupadd snort
+useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
+
+# Create the Snort directories:
+mkdir /etc/snort 
+mkdir /etc/snort/rules
+mkdir /etc/snort/rules/iplists
+mkdir /etc/snort/preproc_rules
+mkdir /usr/local/lib/snort_dynamicrules
+mkdir /etc/snort/so_rules
+ 
+# Create some files that stores rules and ip lists
+touch /etc/snort/rules/iplists/black_list.rules
+touch /etc/snort/rules/iplists/white_list.rules
+touch /etc/snort/rules/local.rules
+touch /etc/snort/sid-msg.map
+ 
+# Create our logging directories:
+mkdir /var/log/snort
+mkdir /var/log/snort/archived_logs
+ 
+# Adjust permissions:
+chmod -R 5775 /etc/snort
+chmod -R 5775 /var/log/snort
+chmod -R 5775 /var/log/snort/archived_logs
+chmod -R 5775 /etc/snort/so_rules
+chmod -R 5775 /usr/local/lib/snort_dynamicrules
+ 
+# Change Ownership on folders:
+chown -R snort:snort /etc/snort
+chown -R snort:snort /var/log/snort
+chown -R snort:snort /usr/local/lib/snort_dynamicrules
+
+cd ~/snort_src/snort-2.9.12/etc/snort
+cp *.conf* /etc/snort
+cp *.map /etc/snort
+cp *.dtd /etc/snort
+
+cd ~/snort_src/snort-2.9.12/src/dynamic-preprocessors/build/usr/local/lib/snort_dynamicpreprocessor/
+cp * /usr/local/lib/snort_dynamicpreprocessor/
